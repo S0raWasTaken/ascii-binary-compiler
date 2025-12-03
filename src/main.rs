d91d45d9da372fd0eeb4b9605b62ae1fb4a77b5e
S0ra <s0ra@duck.com>
2025-12-03 01:10:05 -0500

    First commit
    Easily convert from .bapple to ELF or .exe :)

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+#![warn(clippy::pedantic)]
+
+use std::{
+    ffi::OsStr,
+    fs::{File, copy, create_dir_all},
+    io::Write,
+    path::{Path, PathBuf},
+    process::Command,
+};
+
+use clap::{Parser, crate_version};
+use tempfile::{TempDir, tempdir_in};
+
+const TEMPLATE: [&str; 2] = [
+    include_str!("../template/src/main.rs"),
+    include_str!("../template/Cargo.toml"),
+];
+
+#[derive(Debug, Parser)]
+#[command(version(crate_version!()))]
+struct Args {
+    /// .bapple file to link
+    file: PathBuf,
+
+    /// Temporary directory to be used and discarded right after
+    #[arg(short, long, default_value = ".")]
+    temp_dir: PathBuf,
+}
+
+type Res<T> = Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Res<()> {
+    let args = Args::parse();
+    let file = args.file;
+    let file_clone = file.clone();
+    let file_path = file.parent().unwrap_or(Path::new("."));
+    let file_name = file
+        .file_stem()
+        .unwrap_or(OsStr::new("output"))
+        .to_string_lossy();
+
+    let temp_dir = tempdir_in(args.temp_dir)?;
+
+    copy_template(&temp_dir, &file_name)?;
+
+    copy(file_clone, temp_dir.path().join("link.bapple"))?;
+
+    run_cargo_build(&temp_dir)?;
+    copy(
+        temp_dir.path().join(format!("target/release/{file_name}")),
+        file_path.join(&*file_name),
+    )?;
+
+    Ok(())
+}
+
+fn run_cargo_build(dir: &TempDir) -> Res<()> {
+    let status = Command::new("cargo")
+        .args(["build", "--release"])
+        .current_dir(dir.path())
+        .status()?;
+
+    if !status.success() {
+        return Err("'cargo build --release' failed".into());
+    }
+
+    Ok(())
+}
+
+fn copy_template(dir: &TempDir, file_name: &str) -> Res<()> {
+    let path = dir.path().to_owned();
+
+    let main_rs = TEMPLATE[0].replace(
+        "TEMPLATE_PWD",
+        &path.canonicalize()?.join("link.bapple").to_string_lossy(),
+    );
+    let cargo_toml = TEMPLATE[1].replace("template_name", file_name);
+
+    // Create src/main.rs
+    create_dir_all(path.join("src"))?;
+    let mut file = File::create_new(path.join("src/main.rs"))?;
+
+    file.write_all(main_rs.as_bytes())?;
+
+    // Create Cargo.toml
+    let mut file = File::create_new(path.join("Cargo.toml"))?;
+    file.write_all(cargo_toml.as_bytes())?;
+
+    Ok(())
+}
---
+++ template/src/main.rs
@@ -0,0 +1,58 @@
+use std::{
+    io::{Cursor, Write, stdout},
+    thread::sleep,
+    time::{Duration, Instant},
+};
+
+use ascii_linker::embed_full;
+use rodio::{Decoder, OutputStreamBuilder, Sink, Source};
+
+const FILE: (&[&[u8]], &[u8], u64) = embed_full!("TEMPLATE_PWD");
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (frames, audio, frametime) = FILE;
+    let frametime = Duration::from_micros(frametime);
+
+    let output_stream = OutputStreamBuilder::open_default_stream()?;
+    let decoder = Decoder::new_mp3(Cursor::new(audio))?;
+    let total = decoder.total_duration().unwrap();
+
+    let source = decoder.track_position();
+
+    let sink = Sink::connect_new(output_stream.mixer());
+
+    sink.append(source);
+    sink.play();
+
+    let mut lock = stdout().lock();
+
+    let len = frames.len();
+
+    let mut counter = 0;
+
+    while counter < len {
+        let task_time = Instant::now();
+        let decompressed_frame = zstd::decode_all(frames[counter])?;
+
+        lock.write_all(b"\r\x1b[2J\r\x1b[H")?;
+        lock.write_all(&decompressed_frame)?;
+        lock.flush()?;
+
+        if counter.is_multiple_of(15) {
+            counter = get_pos(len, &sink, total);
+        } else {
+            counter += 1;
+        }
+
+        let elapsed = task_time.elapsed();
+        if elapsed < frametime {
+            sleep(frametime - elapsed);
+        }
+    }
+
+    Ok(())
+}
+
+fn get_pos(len: usize, sink: &Sink, total: Duration) -> usize {
+    (sink.get_pos().div_duration_f64(total) * len as f64).round() as usize
+}
